bea48ef901ae14c15cc300ca2b8b7938c5aaf070
AuraRizz <205314701+AuraRizz@users.noreply.github.com>
2025-05-09 23:35:39 +0200

    Errors arreglats

--- animationplayer/Escenes/main.gd
+++ animationplayer/Escenes/main.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 
 func _on_button_pressed() -> void:
-	$AnimationPlayer.play("prova")
+	$AnimationPlayer.play("RESET")
 
 
-func _on_animation_player_animation_finished(anim_name) -> void:
-	$AnimationPlayer.play("prova")
+#func _on_animation_player_animation_finished(anim_name) -> void:
+	#$AnimationPlayer.play("RESET")
